8feb6211f9edb3a47662f02571b4e34f9be3f521
oleksandr.samoilenko <oleksandr.samoilenko@extrawest.com>
2024-09-24 18:31:09 +0300

    refactor project structure

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,63 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'app.dart';
+import 'common/injector_module.dart';
+import 'common/widgets/global_bloc_provider.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+Future<void> main() async {
+  await InjectorModule.inject();
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme
-            .of(context)
-            .colorScheme
-            .inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Initial Flutter Project',
-            ),
-
-          ],
-        ),
-      ),
-    );
-  }
+  runApp(
+    const GlobalBlocProvider(
+      child: Application(),
+    ),
+  );
 }
