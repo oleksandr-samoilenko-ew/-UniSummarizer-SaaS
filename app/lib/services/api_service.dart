18bc31f6e2b708c67f784bd3fa6fa3870a30fc4e
oleksandr.samoilenko <oleksandr.samoilenko@extrawest.com>
2024-09-25 13:39:18 +0300

    add website summarize call

--- app/lib/services/api_service.dart
+++ app/lib/services/api_service.dart
@@ -3,28 +3,59 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  final String baseUrl = 'http://localhost:3001/fetch-subtitles';
+  final String baseUrl = 'http://localhost:3000';
 
-  Future<String> fetchSubtitles(String url) async {
-    String subtitles = '';
+  Future<String> summarizeYoutube({
+    required String url,
+    required String apiKey,
+  }) async {
+    String summary = '';
 
     final response = await http.post(
-      Uri.parse(baseUrl),
+      Uri.parse('$baseUrl/fetch-subtitles'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
       body: jsonEncode(<String, String>{
         'url': url,
+        'apiKey': apiKey,
       }),
     );
 
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
 
-      subtitles = data['subtitles'].toString();
+      summary = data['summary'].toString();
     } else {
-      subtitles = 'Failed to fetch subtitles';
+      summary = 'Failed to fetch summary';
     }
-    return subtitles;
+    return summary;
+  }
+
+  Future<String> summarizeWebsite({
+    required String url,
+    required String apiKey,
+  }) async {
+    String summary = '';
+    print('apiKey: $apiKey');
+    final response = await http.post(
+      Uri.parse('$baseUrl/summarize-website'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'url': url,
+        'apiKey': apiKey,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+
+      summary = data['summary'].toString();
+    } else {
+      summary = 'Failed to fetch summary';
+    }
+    return summary;
   }
 }
